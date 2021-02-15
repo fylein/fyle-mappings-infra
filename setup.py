3ef4608669c99c899345ccedd9d22344ad0fbe72
ashwinthanaraj <ashwin.t@fyle.in>
2021-02-15 19:02:14 +0530

    Bump up version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='fyle-accounting-mappings',
-    version='0.8.0',
+    version='0.9.0',
     author='Shwetabh Kumar',
     author_email='shwetabh.kumar@fyle.in',
     description='Django application to store the fyle accounting mappings in a generic manner',
