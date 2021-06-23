c18b788edeaf81624f0dfe87fa31c2f423b0eaa5
Sravan Kumar <sravan.kumar@fyle.in>
2021-06-23 11:06:24 +0530

    Adding mapping settings view

--- fyle_accounting_mappings/views.py
+++ fyle_accounting_mappings/views.py
@@ -1,15 +1,46 @@
 import logging
+from typing import Dict, List
 
 from rest_framework.generics import ListCreateAPIView
 from rest_framework.response import Response
 from rest_framework.views import status
 from django.db.models import Count
 
+from .exceptions import BulkError
 from .utils import assert_valid
-from .models import Mapping, ExpenseAttribute, DestinationAttribute
-from .serializers import MappingSerializer
+from .models import MappingSetting, Mapping, ExpenseAttribute, DestinationAttribute
+from .serializers import MappingSettingSerializer, MappingSerializer
 
 logger = logging.getLogger(__name__)
+
+
+class MappingSettingsView(ListCreateAPIView):
+    """
+    Mapping Settings VIew
+    """
+    serializer_class = MappingSettingSerializer
+
+    def get_queryset(self):
+        return MappingSetting.objects.filter(workspace_id=self.kwargs['workspace_id'])
+
+    def post(self, request, *args, **kwargs):
+        """
+        Post mapping settings
+        """
+        try:
+            mapping_settings: List[Dict] = request.data
+
+            assert_valid(mapping_settings != [], 'Mapping settings not found')
+
+            mapping_settings = MappingSetting.bulk_upsert_mapping_setting(mapping_settings, self.kwargs['workspace_id'])
+
+            return Response(data=self.serializer_class(mapping_settings, many=True).data, status=status.HTTP_200_OK)
+        except BulkError as exception:
+            logger.error(exception.response)
+            return Response(
+                data=exception.response,
+                status=status.HTTP_400_BAD_REQUEST
+            )
 
 
 class MappingsView(ListCreateAPIView):
