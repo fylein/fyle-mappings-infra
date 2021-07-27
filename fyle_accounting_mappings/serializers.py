785f97aa35305330b485745ec7562fc385d9d046
ashwinthanaraj <ashwin.t@fyle.in>
2021-07-27 11:53:36 +0530

    Reset changes additional fixes

--- fyle_accounting_mappings/serializers.py
+++ fyle_accounting_mappings/serializers.py
@@ -9,13 +9,13 @@ class ExpenseAttributeSerializer(serializers.ModelSerializer):
     """
     Expense Attribute serializer
     """
-    id = serializers.IntegerField(write_only=True)
+    # id = serializers.IntegerField(write_only=True)
 
     class Meta:
         model = ExpenseAttribute
         fields = '__all__'
         read_only_fields = (
-            'value', 'attribute_type', 'source_id', 'workspace', 'detail',
+            'id', 'value', 'attribute_type', 'source_id', 'workspace', 'detail',
             'auto_mapped', 'auto_created', 'active', 'display_name'
         )
 
@@ -24,13 +24,13 @@ class DestinationAttributeSerializer(serializers.ModelSerializer):
     """
     Destination Attribute serializer
     """
-    id = serializers.IntegerField(write_only=True)
+    # id = serializers.IntegerField(write_only=True)
 
     class Meta:
         model = DestinationAttribute
         fields = '__all__'
         read_only_fields = (
-            'value', 'attribute_type', 'destination_id', 'workspace', 'detail',
+            'id', 'value', 'attribute_type', 'destination_id', 'workspace', 'detail',
             'auto_created', 'active', 'display_name'
         )
 
@@ -61,9 +61,9 @@ class EmployeeMappingSerializer(serializers.ModelSerializer):
     Mapping serializer
     """
     source_employee = ExpenseAttributeSerializer(required=True)
-    destination_employee = DestinationAttributeSerializer()
-    destination_vendor = DestinationAttributeSerializer()
-    destination_card_account = DestinationAttributeSerializer()
+    destination_employee = DestinationAttributeSerializer(required=False)
+    destination_vendor = DestinationAttributeSerializer(required=False)
+    destination_card_account = DestinationAttributeSerializer(required=False)
 
     class Meta:
         model = EmployeeMapping
@@ -80,19 +80,38 @@ class EmployeeMappingSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError('No attribute found with this attribute id')
         return source_employee
 
+    def validate(self, data):
+        return data
+
+    def run_validation(self, data):
+        return data
+
+    def run_validators(self, value):
+        return value
+
     def create(self, validated_data):
         """
         Validated Data to be created
         :param validated_data:
         :return: Created Entry
         """
-        employee_mapping = EmployeeMapping.objects.update_or_create(
-            source_employee=validated_data['source_employee']['id'],
-            workspace=validated_data['workspace'],
+        destination_employee = validated_data['destination_employee']['id'] \
+            if 'destination_employee' in validated_data and validated_data['destination_employee']['id'] else None
+
+        destination_vendor = validated_data['destination_vendor']['id'] \
+            if 'destination_vendor' in validated_data and validated_data['destination_vendor']['id'] else None
+
+        destination_card_account = validated_data['destination_card_account']['id'] \
+            if 'destination_card_account' in validated_data and \
+                validated_data['destination_card_account']['id'] else None
+
+        employee_mapping, _ = EmployeeMapping.objects.update_or_create(
+            source_employee_id=validated_data['source_employee']['id'],
+            workspace_id=validated_data['workspace'],
             defaults={
-                'destination_employee': validated_data['destination_employee']['id'],
-                'destination_vendor': validated_data['destination_vendor']['id'],
-                'destination_card_account': validated_data['destination_card_account']['id']
+                'destination_employee_id': destination_employee,
+                'destination_vendor_id': destination_vendor,
+                'destination_card_account_id': destination_card_account
             }
         )
 
