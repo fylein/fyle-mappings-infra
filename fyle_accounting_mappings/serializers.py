63d58f060117b0e516a27f29460ab10bb8bc1c7c
Shwetabh Kumar <shwetabh002@gmail.com>
2021-07-27 14:01:35 +0530

    Working version of Mapping upsert

--- fyle_accounting_mappings/serializers.py
+++ fyle_accounting_mappings/serializers.py
@@ -2,25 +2,37 @@
 Mapping Serializers
 """
 from rest_framework import serializers
-from .models import ExpenseAttribute, DestinationAttribute, Mapping, MappingSetting
+from .models import ExpenseAttribute, DestinationAttribute, Mapping, MappingSetting, EmployeeMapping
 
 
 class ExpenseAttributeSerializer(serializers.ModelSerializer):
     """
     Expense Attribute serializer
     """
+    id = serializers.IntegerField()
+
     class Meta:
         model = ExpenseAttribute
         fields = '__all__'
+        read_only_fields = (
+            'value', 'attribute_type', 'source_id', 'workspace', 'detail',
+            'auto_mapped', 'auto_created', 'active', 'display_name'
+        )
 
 
 class DestinationAttributeSerializer(serializers.ModelSerializer):
     """
     Destination Attribute serializer
     """
+    id = serializers.IntegerField(allow_null=True)
+
     class Meta:
         model = DestinationAttribute
         fields = '__all__'
+        read_only_fields = (
+            'value', 'attribute_type', 'destination_id', 'workspace', 'detail',
+            'auto_created', 'active', 'display_name'
+        )
 
 
 class MappingSettingSerializer(serializers.ModelSerializer):
@@ -42,3 +54,46 @@ class MappingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Mapping
         fields = '__all__'
+
+
+class EmployeeMappingSerializer(serializers.ModelSerializer):
+    """
+    Mapping serializer
+    """
+    source_employee = ExpenseAttributeSerializer(required=True)
+    destination_employee = DestinationAttributeSerializer()
+    destination_vendor = DestinationAttributeSerializer()
+    destination_card_account = DestinationAttributeSerializer()
+
+    class Meta:
+        model = EmployeeMapping
+        fields = '__all__'
+
+    def validate_source_employee(self, source_employee):
+        attribute = ExpenseAttribute.objects.filter(
+            id=source_employee['id'],
+            workspace_id=self.initial_data['workspace'],
+            attribute_type='EMPLOYEE'
+        ).first()
+
+        if not attribute:
+            raise serializers.ValidationError('No attribute found with this attribute id')
+        return source_employee
+
+    def create(self, validated_data):
+        """
+        Validated Data to be created
+        :param validated_data:
+        :return: Created Entry
+        """
+        employee_mapping, _ = EmployeeMapping.objects.update_or_create(
+            source_employee_id=validated_data['source_employee']['id'],
+            workspace=validated_data['workspace'],
+            defaults={
+                'destination_employee_id': validated_data['destination_employee']['id'],
+                'destination_vendor_id': validated_data['destination_vendor']['id'],
+                'destination_card_account_id': validated_data['destination_card_account']['id']
+            }
+        )
+
+        return employee_mapping
