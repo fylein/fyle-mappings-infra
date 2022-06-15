e425997d20bc5c2a356ba852d28c7ea5e5cf4717
ashwin1111 <winash1998@gmail.com>
2022-06-15 18:43:27 +0530

    Include numbers as well in the mappings list

--- fyle_accounting_mappings/views.py
+++ fyle_accounting_mappings/views.py
@@ -226,7 +226,7 @@ class ExpenseAttributesMappingView(ListAPIView):
         if all_alphabets == 'true':
             mapping_source_alphabets = [
                 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
-                'V', 'W', 'X', 'Y', 'Z'
+                'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
             ]
 
         if mapped and mapped.lower() == 'false':
