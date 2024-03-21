7a94be02e18bd9a9227415ca19474db3ba38fae0
ruuushhh <66899387+ruuushhh@users.noreply.github.com>
2024-03-21 14:00:56 +0530

    handle delete case for categories and projects in expense attributes (#105)
    * handle delete case for categories and projects in expense attributes
    
    * fix lint
    
    * fix lint

--- fyle_accounting_mappings/models.py
+++ fyle_accounting_mappings/models.py
@@ -3,6 +3,7 @@ from typing import List, Dict
 from datetime import datetime
 from django.db import models, transaction
 from django.db.models import JSONField
+from django.contrib.postgres.fields import ArrayField
 
 from .exceptions import BulkError
 from .utils import assert_valid
@@ -106,6 +107,16 @@ def get_existing_source_ids(destination_type: str, workspace_id: int):
     return existing_source_ids
 
 
+class ExpenseAttributesDeletionCache(models.Model):
+    id = models.AutoField(primary_key=True)
+    category_ids = ArrayField(default=[], base_field=models.CharField(max_length=255))
+    project_ids = ArrayField(default=[], base_field=models.CharField(max_length=255))
+    workspace = models.OneToOneField(Workspace, on_delete=models.PROTECT, help_text='Reference to Workspace model')
+
+    class Meta:
+        db_table = 'expense_attributes_deletion_cache'
+
+
 class ExpenseAttribute(models.Model):
     """
     Fyle Expense Attributes
@@ -145,6 +156,42 @@ class ExpenseAttribute(models.Model):
             }
         )
         return expense_attribute
+
+    @staticmethod
+    def bulk_update_deleted_expense_attributes(attribute_type: str, workspace_id: int):
+        """
+        Bulk update deleted expense attributes
+        :param attribute_type: Attribute type
+        :param workspace_id: Workspace Id
+        """
+        expense_attributes_deletion_cache = ExpenseAttributesDeletionCache.objects.get(workspace_id=workspace_id)
+        attributes_to_be_updated = []
+
+        if attribute_type == 'CATEGORY':
+            deleted_attributes = ExpenseAttribute.objects.filter(
+                attribute_type=attribute_type, workspace_id=workspace_id, active=True
+            ).exclude(source_id__in=expense_attributes_deletion_cache.category_ids)
+            expense_attributes_deletion_cache.category_ids = []
+            expense_attributes_deletion_cache.save()
+        else:
+            deleted_attributes = ExpenseAttribute.objects.filter(
+                attribute_type=attribute_type, workspace_id=workspace_id, active=True
+            ).exclude(source_id__in=expense_attributes_deletion_cache.project_ids)
+            expense_attributes_deletion_cache.project_ids = []
+            expense_attributes_deletion_cache.save()
+
+        for attribute in deleted_attributes:
+            attributes_to_be_updated.append(
+                ExpenseAttribute(
+                    id=attribute.id,
+                    active=False,
+                    updated_at=datetime.now()
+                )
+            )
+
+        if attributes_to_be_updated:
+            ExpenseAttribute.objects.bulk_update(
+                attributes_to_be_updated, fields=['active', 'updated_at'], batch_size=50)
 
     @staticmethod
     def bulk_create_or_update_expense_attributes(
