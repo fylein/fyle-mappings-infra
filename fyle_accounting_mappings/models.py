b7a49f913b0e91be500d31ae83816df662269797
Ashutosh619-sudo <ashutoshsinghrkt@gmail.com>
2024-12-24 19:57:53 +0530

    fixing lint

--- fyle_accounting_mappings/models.py
+++ fyle_accounting_mappings/models.py
@@ -424,14 +424,15 @@ class DestinationAttribute(models.Model):
                 )
             else:
                 if attribute_disable_callback_path and is_import_to_fyle_enabled and (
-                    (attribute['value'] and primary_key_map[attribute['destination_id']]['value'] and attribute['value'].lower(
-                    ) != primary_key_map[attribute['destination_id']]['value'].lower())
-                    or ('code' in attribute and attribute['code'] and attribute['code'] != primary_key_map[attribute['destination_id']]['code'])
+                    (attribute['value'] and primary_key_map[attribute['destination_id']]['value'] and
+                    attribute['value'].lower() != primary_key_map[attribute['destination_id']]['value'].lower()) or
+                    ('code' in attribute and attribute['code'] and
+                    attribute['code'] != primary_key_map[attribute['destination_id']]['code'])
                 ):
                     attributes_to_disable[attribute['destination_id']] = {
-                        'value': primary_key_map[attribute['destination_id']]['value'],
+                        'value': primary_key_map['destination_id']['value'],
                         'updated_value': attribute['value'],
-                        'code': primary_key_map[attribute['destination_id']]['code'],
+                        'code': primary_key_map['destination_id']['code'],
                         'updated_code': attribute['code']
                     }
 
@@ -456,7 +457,10 @@ class DestinationAttribute(models.Model):
 
         if attribute_disable_callback_path and attributes_to_disable:
             import_string(attribute_disable_callback_path)(
-                workspace_id, attributes_to_disable, is_import_to_fyle_enabled)
+                workspace_id,
+                attributes_to_disable,
+                is_import_to_fyle_enabled
+            )
 
         if attributes_to_be_created:
             DestinationAttribute.objects.bulk_create(
